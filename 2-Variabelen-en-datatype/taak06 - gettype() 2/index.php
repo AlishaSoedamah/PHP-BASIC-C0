ce5ace9cb28c2b32b2e1c706ff2f61a55f26891a
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-12-04 12:15:11 +0100

    taak6

---
+++ 2-Variabelen-en-datatype/taak06 - gettype() 2/index.php
@@ -0,0 +1,24 @@
+<?php
+ $voornaam = "Tom";
+ echo gettype($voornaam);
+ ?>
+ <br>
+ <?php
+ $age =  16;
+ echo gettype($age)
+?>
+ <br>
+ <?php
+ $agesecond =  16.66;
+ echo gettype($agesecond)
+?>
+ <br>
+ <?php
+ $boo =  true;
+ echo gettype($boo)
+?>
+ <br>
+ <?php
+ $zero =  null;
+ echo gettype($zero)
+?>
