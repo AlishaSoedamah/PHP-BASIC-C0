a9125966e0d8fa5681dff26089715c104b3abddb
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2020-12-05 14:59:25 +0100

    Rekenen met php

---
+++ 4-Arithmetics/taak01 - Arithmetic Operators/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+    <?php
+         $getal1 = 4;
+         $getal2 = 99;
+         echo $getal1 + $getal2;
+    ?>
+    </p>
+    <p>
+    <?php
+         $getal3 = 100;
+         $getal4 = 99;
+         echo $getal3 - $getal4;
+    ?>
+    </p>
+    <?php
+         $getal5 = 100;
+         $getal6 = 2;
+         echo $getal5 * $getal6;
+    ?>
+    </p>
+    <?php
+         $getal7 = 100;
+         $getal8 = 2;
+         echo $getal7 / $getal8;
+    ?>
+    </p>
+</body>
+</html>
---
+++ 4-Arithmetics/taak02 -Assignment Operators/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p>
+    <?php
+    $getal = 20;
+    echo $getal += 20
+    ?>
+    </p>
+    <p>
+    <?php
+    echo $getal -= 20
+    ?>
+    </p>
+    <p>
+    <?php
+    echo $getal *= 20
+    ?>
+    </p>
+    <p>
+    <?php
+    echo $getal /= 40;
+    ?>
+    </p>
+</body>
+</html>
